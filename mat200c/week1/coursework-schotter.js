16e7617f306bfd6a8719da689c323a30422f7749
nworb9 <emma.nworb@gmail.com>
2023-10-01 19:08:01 -0700

    Ugh I give up lol

--- mat200c/week1/coursework-schotter.js
+++ mat200c/week1/coursework-schotter.js
@@ -1,64 +1,79 @@
+let gridHeight = 22;
+let gridWidth = 12;
+
+let count = 0;
+
+let currentAngle = 0;
+let angleIncrement = 10;
+let exponentialBaseAngle = 1.035;
+let angleDampingFactor = 800;
+
+let jitter = 10;
+let jitterDampingFactor = 50;
 
 function setup() {
-  createCanvas(400, 700);
+  createCanvas(500, 800);
   noLoop();
   rectMode(CENTER);
   angleMode(RADIANS);
-  // I used chatGPT to simplify the code (it 
+  // I used chatGPT to simplify the code (it
   // came up with the clever random direction ternary)
-  // make the angle change exponential.
+  // and how to make the angle change exponential.  It also suggested
+  // a dampingFactor when I was struggling
+  // to make the positional shifts grow more slowly.
+
   // I decided to try randomGaussian() after
-  // being unsatisfied with the symmetrical 
-  // looking results from just random() + jitterIncrement
-  // (it looked like it was rotating back and forth too evenly)
+  // being unsatisfied with the symmetrical
+  // looking results from just random()
+  // (it looked like it was shifting back and forth too evenly)
 }
 
 function draw() {
   background(255);
   fill(255, 0);
-  
-  let gridHeight = 22;
-  let gridWidth = 12;
-
-  let count = 0;
-
-  let currentAngle = 0;
-  let angleIncrement = 0.02;
-
-  let exponentialBase = 1.035;
-  let exponentialBaseJitter = 1.02;
-
-  let xJitter = 10;
-  let yJitter = 10;
-  let jitterIncrement = 0.02;
 
   while (count < gridHeight * gridWidth) {
     for (let row = 0; row < gridHeight; row++) {
       for (let column = 0; column < gridWidth; column++) {
         let direction = random() < 0.5 ? 1 : -1;
-        
-        
-        let xShift =
+
+        let rawXShift =
           direction *
-          xJitter *
-          randomGaussian(jitterIncrement, jitterIncrement) *
-          Math.pow(exponentialBaseJitter, count);
-        
-        let yShift =
+          jitter *
+          randomGaussian(
+            0,
+            Math.log(count + 1)
+          );
+        let xShift = rawXShift / jitterDampingFactor;
+
+        let rawYShift =
           direction *
-          yJitter *
-          randomGaussian(jitterIncrement, jitterIncrement) *
-          Math.pow(exponentialBaseJitter, count);
+          jitter *
+          randomGaussian(
+            0,
+            Math.log(count + 1)
+          );
+        let yShift = rawYShift / jitterDampingFactor;
 
         push();
-        translate(20 + column * 30 + xShift, 20 + row * 30 + yShift);
+
+        let xPosition = 70 + column * 30 + xShift;
+        let yPosition = 70 + row * 30 + yShift;
+        console.log(xShift);
+        console.log(yShift);
+        translate(xPosition, yPosition);
 
         rotate(
           currentAngle +
-            direction * angleIncrement * Math.pow(exponentialBase, count)
+            (direction *
+              angleIncrement *
+              Math.pow(exponentialBaseAngle, count)) /
+              angleDampingFactor
         );
 
-        rect(0, 0, 30, 30);
+        // I copied this and the translate call from you, it was
+        // more readable than my early attempts with `square()`
+        rect(15, 15, 30, 30);
 
         pop();
 
@@ -67,4 +82,5 @@ function draw() {
     }
   }
 }
+
 
