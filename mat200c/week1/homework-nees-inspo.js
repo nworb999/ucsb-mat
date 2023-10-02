ed3a48177415c79c9dfbd39af33f945b937d60d4
emma <emma.nworb@gmail.com>
2023-10-02 17:35:36 -0400

    Update homework-nees-inspo.js

--- mat200c/week1/homework-nees-inspo.js
+++ mat200c/week1/homework-nees-inspo.js
@@ -3,7 +3,9 @@
 // I like this program because of the range of outcomes it can produce
 
 // I used a markov chain because I had been researching it for another
-// project and I wanted more symmetry in the results
+// project and I wanted more symmetry in the results.  
+
+// The boilerplate for markov chain from chatGPT.
 
 let maxRadius = 200;
 let radiusShift = 10;
@@ -15,7 +17,6 @@ let state;
 let counter = 0;
 let maxCalls = 12 * 22; // same as nees
 
-// boilerplate for markov chain from chatGPT
 
 // transition matrix, two states, i and j
 let matrix = [
@@ -33,9 +34,9 @@ function setup() {
 
   x = width / 2;
   y = height / 2;
-  r = 50; // radius of the circle
+  r = 50;
 
-  state = 0; // initial state
+  state = 0; 
 }
 
 function draw() {
@@ -44,33 +45,27 @@ function draw() {
 
     fill(255, 0);
 
-    // draw the circle
     ellipse(min(400, x), min(700, y), r, r);
 
-    // generate random for position shift
     let randomValue = random([0, 1]);
 
-    // markov chain transition using transition matrix
     let nextState = getNextState(state, matrix);
 
-    // apply distortion based on the state
     switch (nextState) {
       case 0:
-        r > maxRadius ? (r = maxRadius) : (r += radiusShift); // increase size
+        r > maxRadius ? (r = maxRadius) : (r += radiusShift);
         break;
       case 1:
-        r < 10 ? (r = 10) : (r -= radiusShift); // decrease size but don't let it disappear
-        // (not letting it disappear is from chatGPT)
+        r < 10 ? (r = 10) : (r -= radiusShift); // (decreasing size but not letting it disappear is from chatGPT)
         break;
       case 2:
-        randomValue === 1 ? (x -= skewShift) : (x += skewShift); // skew horizontally
+        randomValue === 1 ? (x -= skewShift) : (x += skewShift); 
         break;
       case 3:
-        randomValue == 1 ? (y -= skewShift) : (y += skewShift); // skew vertically
+        randomValue == 1 ? (y -= skewShift) : (y += skewShift);
         break;
     }
 
-    // update the state
     state = nextState;
   }
 
@@ -85,7 +80,7 @@ function draw() {
       }
     }
 
-    return currentState; // fallback to currentState if something goes wrong
+    return currentState;
   }
 }
 
