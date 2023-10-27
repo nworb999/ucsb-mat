28a68276e9c403a8205b282df0ccbdf236bc0910
nworb9 <emma.nworb@gmail.com>
2023-10-27 11:04:14 -0700

    More starter code

--- mat240a/week5/monotron/monotron.ino
+++ mat240a/week5/monotron/monotron.ino
@@ -5,21 +5,21 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioSynthWaveform       waveform1;      //xy=82,44
-AudioSynthWaveformModulated waveformMod1;   //xy=203,135
-AudioMixer4              mixer1;         //xy=423,133
+AudioSynthWaveform       LFO;      //xy=82,44
+AudioSynthWaveformModulated VCO;   //xy=203,135
+AudioMixer4              feedback;         //xy=423,133
 AudioEffectDelay         delay1;         //xy=463,366
-AudioFilterLadder        filter;        //xy=571,139
-AudioAmplifier           amp1;           //xy=728,141
-AudioOutputI2S           i2s1;           //xy=896,157
-AudioConnection          patchCord1(waveform1, 0, waveformMod1, 0);
-AudioConnection          patchCord2(waveformMod1, 0, mixer1, 0);
-AudioConnection          patchCord3(mixer1, 0, filter, 0);
-AudioConnection          patchCord4(delay1, 0, mixer1, 1);
-AudioConnection          patchCord5(filter, delay1);
-AudioConnection          patchCord6(filter, amp1);
-AudioConnection          patchCord7(amp1, 0, i2s1, 0);
-AudioConnection          patchCord8(amp1, 0, i2s1, 1);
+AudioFilterLadder        VCF;        //xy=571,139
+AudioAmplifier           volume;           //xy=728,141
+AudioOutputI2S           output;           //xy=896,157
+AudioConnection          patchCord1(LFO, 0, VCO, 0);
+AudioConnection          patchCord2(VCO, 0, feedback, 0);
+AudioConnection          patchCord3(feedback, 0, VCF, 0);
+AudioConnection          patchCord4(delay1, 0, feedback, 1);
+AudioConnection          patchCord5(VCF, delay1);
+AudioConnection          patchCord6(VCF, volume);
+AudioConnection          patchCord7(volume, 0, output, 0);
+AudioConnection          patchCord8(volume, 0, output, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=895,70
 // GUItool: end automatically generated code
 
