eac62e670f6c0c37afce73b313d48276e38a6b74
nworb9 <emma.nworb@gmail.com>
2023-11-05 23:47:18 -0800

    OK for now

--- mat200c/week6/vehicles.js
+++ mat200c/week6/vehicles.js
@@ -3,15 +3,21 @@ const Temperament = {
   EXTROVERT: "extrovert",
 };
 
+function randomSpeed() {
+  return (random(1) > 0.5 ? 1 : -1) * random(3, 5);
+}
+
 class Character {
   constructor(temperament, confidence, energyLevel, color) {
     this.position = createVector(random(width), random(height));
-    this.velocity = createVector(random(-1, 1), random(-1, 1));
+    this.velocity = createVector(randomSpeed(), randomSpeed());
     this.temperament = temperament;
-    this.confidence = confidence; // Initially set to 1-5
+    this.confidence = confidence;
+    this.targetConfidence = confidence; // The desired confidence level after interactions (thanks chatGPT)
     this.energyLevel = energyLevel; // 1-10
     this.color = color;
     this.crush = null;
+    this.interactedThisFrame = false;
   }
 
   applyForce(force) {
@@ -28,7 +34,12 @@ class Character {
     if (this.crush) {
       stroke(this.getColor(), 75);
       strokeWeight(2);
-      line(offset, 0, this.crush.position.x - this.position.x + offset, this.crush.position.y - this.position.y);
+      line(
+        offset,
+        0,
+        this.crush.position.x - this.position.x + offset,
+        this.crush.position.y - this.position.y
+      );
     }
 
     fill(this.getColor());
@@ -55,6 +66,9 @@ class Character {
   }
 
   update() {
+    let velocityModifier = map(this.energyLevel, 1, 10, 0.5, 1.5);
+    this.velocity.mult(velocityModifier);
+    
     if (this.crush) {
       let attractionForce = p5.Vector.sub(this.crush.position, this.position);
       let distanceSq = attractionForce.magSq();
@@ -64,40 +78,84 @@ class Character {
       this.applyForce(attractionForce);
       this.velocity.mult(softeningFactor);
     }
-
+    this.velocity.x = constrain(this.velocity.x, -maxSpeed, maxSpeed);
+    this.velocity.y = constrain(this.velocity.y, -maxSpeed, maxSpeed);
+    
     this.position.add(this.velocity);
     this.constrainToCanvas();
+    this.adjustConfidence();
   }
 
   interact(other) {
     let distance = p5.Vector.dist(this.position, other.position);
     if (distance < this.confidence + other.confidence) {
       this.doInteraction(other);
+      this.interactedThisFrame = true;
+      other.interactedThisFrame = true;
     }
   }
 
   doInteraction(other) {
-    let diceRoll = ceil(random(10)); // Rolling a 10-sided dice
+    let confidenceFactor = map(this.confidence, 10, 50, 0.5, 1.5); // Scale factor based on confidence
+    let diceRoll = ceil(random(10) * confidenceFactor); // Rolling a 10-sided dice
     let successThreshold = 6; // A roll of 6 or higher is a success
-    let interactionOutcome = diceRoll >= successThreshold ? diceRoll - 5 : diceRoll - 6;
-    
-    // Adjust confidence based on the outcome of the interaction
-    this.adjustConfidence(interactionOutcome);
-    other.adjustConfidence(interactionOutcome);
+    let interactionOutcome =
+      diceRoll >= successThreshold ? diceRoll - 5 : diceRoll - 6;
 
-    let forceMagnitude = map(abs(interactionOutcome), 0, 4, 2, 5);
+    this.setTargetConfidence(interactionOutcome);
+    other.setTargetConfidence(interactionOutcome);
+
+    this.adjustEnergy();
+    other.adjustEnergy();
+
+    // make the character bounce away more after a failed interaction
+    let forceMagnitude = map(
+      abs(interactionOutcome),
+      0,
+      4,
+      failForce,
+      succeedForce
+    );
     let repelForce = p5.Vector.sub(this.position, other.position);
     repelForce.setMag(forceMagnitude);
 
     this.applyForce(repelForce);
-    repelForce.mult(-1); // Apply an equal and opposite force to the other character
-    other.applyForce(repelForce);
   }
 
-  adjustConfidence(change) {
-    // Adjust the confidence proportionally to the interaction outcome
-    this.confidence += change;
-    this.confidence = constrain(this.confidence, 1, 10); // Ensure confidence stays within bounds
+  setTargetConfidence(change) {
+    this.targetConfidence += change;
+    this.targetConfidence = constrain(this.targetConfidence, 10, 50);
+  }
+
+  adjustConfidence() {
+    // Gradually adjust the current confidence towards the target confidence
+    if (this.confidence < this.targetConfidence) {
+      this.confidence += this.confidenceAdjustSpeed;
+      if (this.confidence > this.targetConfidence) {
+        this.confidence = this.targetConfidence;
+      }
+    } else if (this.confidence > this.targetConfidence) {
+      this.confidence -= this.confidenceAdjustSpeed;
+      if (this.confidence < this.targetConfidence) {
+        this.confidence = this.targetConfidence;
+      }
+    }
+    let newConfidence = constrain(this.targetConfidence, 10, 50);
+    this.confidence =
+      typeof newConfidence === "number" && !isNaN(newConfidence)
+        ? newConfidence
+        : this.confidence;
+  }
+
+  adjustEnergy() {
+    // Introverts regain energy when not interacting, extroverts lose energy
+    if (this.temperament === Temperament.INTROVERT) {
+      this.energyLevel += this.interactedThisFrame ? 0 : 1;
+    } else {
+      this.energyLevel -= this.interactedThisFrame ? 0 : 1;
+    }
+    this.energyLevel = constrain(this.energyLevel, 1, 10);
+    this.interactedThisFrame = false;
   }
 
   getColor() {
@@ -107,26 +165,32 @@ class Character {
 }
 
 let characters = [];
+const maxSpeed = 5;
+const characterCount = 10;
+const failForce = 3;
+const succeedForce = 1;
+const confidenceAdjustSpeed = 0.1;
 const crushAttractionMultiplier = 10;
 const softeningFactor = 0.8;
-const G = 150;
+const G = 1200;
 
 function setup() {
   createCanvas(800, 800);
-  for (let i = 0; i < 4; i++) {
-    let temperament = i % 2 === 0 ? Temperament.INTROVERT : Temperament.EXTROVERT;
+  for (let i = 0; i < characterCount; i++) {
+    let temperament =
+      i % 2 === 0 ? Temperament.INTROVERT : Temperament.EXTROVERT;
     characters.push(
       new Character(
         temperament,
-        random(5, 30),
+        random(10, 30),
         random(1, 10),
         color(random(255), random(255), random(255))
       )
     );
   }
 
-  characters.forEach(char => {
-    let possibleCrushes = characters.filter(c => c !== char);
+  characters.forEach((char) => {
+    let possibleCrushes = characters.filter((c) => c !== char);
     char.crush = random(possibleCrushes);
     let r = p5.Vector.sub(char.crush.position, char.position);
     let distance = r.mag();
