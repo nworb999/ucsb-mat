486bc50b57ce01bbc2650fda3d57b9fce8b18be2
nworb9 <emma.nworb@gmail.com>
2023-11-19 20:31:42 -0800

    Added nodemon for dev and favicon lmao also it's working lfg

--- mat200c/final/public/sketch.js
+++ mat200c/final/public/sketch.js
@@ -1,10 +1,9 @@
-import { Toilet, Table } from "../models/furniture.js";
+import { Toilet, Table } from "./models/furniture.js";
 
-// let game;
-
+let game;
 function setup() {
-  background(100);
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
+  background(0);
 
   let padding = 80;
   let squareSize = (width - padding * 4) / 3;
@@ -13,37 +12,31 @@ function setup() {
 
   const order = "random";
   const alignments = [{ name: "Type 1" }, { name: "Type 2" }];
-  const leftTablePosition = {
+  const leftTable = {
     x: width / 4 - squareSize / 2,
     y: (2 * height) / 5 - squareSize / 2,
     size: squareSize,
   };
-  const rightTablePosition = {
+  const rightTable = {
     x: (3 * width) / 4 - squareSize / 2,
     y: (2 * height) / 5 - squareSize / 2,
     size: squareSize,
   };
-  const bathroomPosition = {
+  const bathroom = {
     centerX: width / 2,
     centerY: (5 * height) / 6 - stallSize / 2,
     stallSize: 50,
   };
 
-  // new Toilet(bathroom);
+  toilet = new Toilet(bathroom);
 
-  // game = new Game(
-  //   order,
-  //   alignments,
-  //   leftTablePosition,
-  //   rightTablePosition,
-  //   bathroomPosition
-  // );
+  game = new Game(order, alignments, leftTable, rightTable, bathroom);
 
   // game.chooseSeats();
 }
 
 function draw() {
-  background(255);
+  // background(255);
   // game.drawFurniture();
   // game.updateCharacters();
 }
